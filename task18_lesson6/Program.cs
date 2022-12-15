2493f4bdd2f6911c3a6e93b98b724133e4842227
perfoi <olga.perfileva11@gmail.com>
2022-12-15 05:45:45 +0300

    GB Homework

---
+++ task18_lesson6/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+// Вводим сразу все числа в одну линию, не через бесконечное кол-во ReadLine.
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+int[,] result = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+result[i, j] = new Random().Next(minValue, maxValue + 1);
+}
+}
+return result;
+}
+void PrintArray(int[,] inArray)
+{
+for (int row = 0; row < inArray.GetLength(0); row++)
+{
+for (int column = 0; column < inArray.GetLength(1); column++)
+{
+Console.Write($"{inArray[row,column]} ");
+}
+Console.WriteLine();
+}
+}
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns=Convert.ToInt32(Console.ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
