f85b3660f3bf2e7c7fcc62ed8b827b621e740674
perfoi <olga.perfileva11@gmail.com>
2022-12-19 22:18:20 +0300

    GB Homework

---
+++ task25_lesson8/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 58: Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+Console.Write("Введите количество строк первого массива: ");
+int rowsA = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов первого массива: ");
+int columnsA = int.Parse(Console.ReadLine());
+Console.Write("Введите количество строк второго массива: ");
+int rowsB = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов второго массива: ");
+int columnsB = int.Parse(Console.ReadLine());
+if (columnsA != rowsB)
+{
+  Console.WriteLine("Матрицы несогласованы, операция умножения, в этом случае, недопустима. ");
+  return;
+}
+    int[,] A = GetArray(rowsA, columnsA, 0, 10);
+    int[,] B = GetArray(rowsB, columnsB, 0, 10);
+    PrintArray(A);
+    Console.WriteLine();
+    PrintArray(B);
+    Console.WriteLine();
+    PrintArray(MultiplicationMatrix(A,B));
+
+    int[,] GetArray(int m, int n, int min, int max)
+    {
+      int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        result[i, j] = new Random().Next(min, max + 1);
+      }
+      
+    }
+      return result;
+    }
+
+    void PrintArray(int[,] inArray)
+    {
+     for (int i = 0; i < inArray.GetLength(0); i++)
+     {
+       for (int j = 0; j < inArray.GetLength(1); j++)
+       {
+         Console.Write($"{inArray[i, j]} ");
+       }
+       Console.WriteLine();
+     }
+    }
+
+    int [,] MultiplicationMatrix(int[,] arrayA, int[,] arrayB)
+    {
+      int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+      for (int i = 0; i < arrayA.GetLength(0); i++)
+      {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+          for (int k = 0; k < arrayA.GetLength(1); k++)
+          {
+            arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+          }
+        }
+      }
+      return arrayC;
+    }
